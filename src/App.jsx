85f64e94ef9a140a17baa4e66a9597ff00f76db7
aliya-sabir <aliyasabir21@gmail.com>
2026-04-10 00:28:06 +0300

    added JobsPage.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,17 @@ import {
   RouterProvider
 } from 'react-router-dom';
 import HomePage from './pages/HomePage.jsx';
-import MainLayout from './layouts/MainLayouts.jsx'
+import MainLayout from './layouts/MainLayouts.jsx';
+import JobsPage from './pages/JobsPage.jsx';
+import NotFoundPage from './pages/NotFoundPage.jsx';
 
 // create router object
 const router = createBrowserRouter(
   createRoutesFromElements(
   <Route path='/' element={<MainLayout />}>
   <Route index element={<HomePage />}/>
+  <Route path='/jobs' element={<JobsPage />} />
+  <Route path='*' element={<NotFoundPage />} />
   </Route>
   )
 );
