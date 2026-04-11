fb4e420819e75add9d024af931f5098c6b3357e3
aliya-sabir <aliyasabir21@gmail.com>
2026-04-11 19:54:06 +0300

    added AddJobPage.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import HomePage from './pages/HomePage.jsx';
 import MainLayout from './layouts/MainLayouts.jsx';
 import JobsPage from './pages/JobsPage.jsx';
 import NotFoundPage from './pages/NotFoundPage.jsx';
+import JobPage, { jobLoader } from './pages/JobPage';
+import AddJobPage from './pages/AddJobPage.jsx';
 
 // create router object
 const router = createBrowserRouter(
@@ -15,6 +17,8 @@ const router = createBrowserRouter(
   <Route path='/' element={<MainLayout />}>
   <Route index element={<HomePage />}/>
   <Route path='/jobs' element={<JobsPage />} />
+  <Route path='/add-job' element={<AddJobPage />} />
+  <Route path='/jobs/:id' element={<JobPage />} loader = {jobLoader} />
   <Route path='*' element={<NotFoundPage />} />
   </Route>
   )
